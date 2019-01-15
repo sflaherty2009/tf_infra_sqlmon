2d3299740b7e6b733b7394a2f03f718cd5f65db1
Scott Flaherty <scott_flaherty@trekbikes.com>
2019-01-15 14:59:16 -0600

    added new lock mechanism to prevent similar locks across infrastructure.

--- resource_group.tf
+++ resource_group.tf
@@ -1,10 +1,12 @@
+resource "random_uuid" "lock" { }
+
 resource "azurerm_resource_group" "rg" {
   name     = "AZ-RG-${upper(lookup(var.penv,terraform.workspace))}-sqlmon"
   location = "${var.location}"
 }
 
-resource "azurerm_management_lock" "prod_sqlmon_lock" {
-  name       = "DoNotDelete"
+resource "azurerm_management_lock" "lock" {
+  name       = "${azurerm_resource_group.rg.name}_lock_${substr(random_uuid.lock.result, -3, -1)}"
   scope      = "${azurerm_resource_group.rg.id}"
   lock_level = "CanNotDelete"
   notes      = "Implemented as part of DVO-3231"
